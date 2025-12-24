6a7744396460ba8e04c2161f63c600f61db0bd4e
medaminsdiri <73961873+kingsdiri007@users.noreply.github.com>
2025-12-25 00:37:35 +0100

    Add files via upload

---
+++ backend/migrations/versions/6360ff7ec216_add_goal_weight_and_other_fields.py
@@ -0,0 +1,24 @@
+"""add goal_weight and other fields
+
+Revision ID: 6360ff7ec216
+Revises: fc67c6f669f7
+Create Date: 2025-12-25 00:33:58.417676
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6360ff7ec216'
+down_revision = 'fc67c6f669f7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ backend/migrations/versions/fc67c6f669f7_manual_fix.py
@@ -0,0 +1,24 @@
+"""manual fix
+
+Revision ID: fc67c6f669f7
+Revises: 6924f5a06d74
+Create Date: 2025-12-25 00:32:05.120394
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fc67c6f669f7'
+down_revision = '6924f5a06d74'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
